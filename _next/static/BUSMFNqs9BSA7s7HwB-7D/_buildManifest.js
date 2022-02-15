9fc98fe1e89dd5e9083b7f949722837ea58d4689
KevinTss <kevin.tassi@gmail.com>
2022-02-15 20:21:55 +0000

    Deploying to gh-pages from @ KevinTss/jules-website@7c2f2b98dae99509afb6688309cdbc3cd2cb9f22 🚀

---
+++ _next/static/BUSMFNqs9BSA7s7HwB-7D/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/0c428ae2-ba8c280468d41662.js","static/chunks/802-d92f505aa77a2adf.js","static/chunks/pages/index-e78f00769788273b.js"],"/_error":["static/chunks/pages/_error-9734db4c9fd59614.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
