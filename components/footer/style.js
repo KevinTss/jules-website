3b08399083df5030351f104f4ec190a37688c023
KevinTss <kevin.tassi@gmail.com>
2022-02-24 18:11:45 +0100

    Fix footer height

--- components/footer/style.js
+++ components/footer/style.js
@@ -4,7 +4,7 @@ export const FooterContainer = styled.footer`
   display: flex;
   width: 100%;
   background: ${({ theme }) => theme.color.background};
-  padding: 50px 0 20px 0;
+  padding: 20px 0 20px 0;
 
   p {
     font-size: 12px;
